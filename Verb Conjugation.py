adecba4f6f15ab24ca5bd5c5cdd30ae692e257d3
Christopher Durr <christopherdurr1@gmail.com>
2014-07-07 18:58:07 -0700

    Update Verb Conjugation.py

--- Verb Conjugation.py
+++ Verb Conjugation.py
@@ -10,11 +10,37 @@ while inputpCorrect == False:
 inputwCorrect = False
 while inputwCorrect == False:
     print
-    word = raw_input("Enter a word: ")
+    verb = raw_input("Enter the infinitive of the verb: ")
     try:
         int(word)
     except:
         inputwCorrect = True
         
-wordRoot = word[:len(word) - 2:]
-wordEnding = word[len(word) - 2:]
+verbRoot = verb[:len(verb) - 2:]
+verbEnding = verb[len(verb) - 2:]
+
+print verbEnding
+
+if pronoun == "yo":
+    conjugatedVerb = verbRoot + "o"
+elif pronoun == "tu":
+    if (verbEnding == "er") or (verbEnding == "ir"):
+        conjugatedVerb = verbRoot + "es"
+    else:
+        conjugatedVerb = verbRoot + "as"
+elif pronoun == "usted"
+    if (verbEnding == "er") or (verbEnding == "ir"):
+        conjugatedVerb = verbRoot + "e"
+    else:
+        conjugatedVerb = verbRoot + "a"
+
+
+
+
+    
+print
+print "The verb conjugated is: " + conjugatedVerb
+
+    
+
+    
